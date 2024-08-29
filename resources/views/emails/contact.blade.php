2b103221df4277f3a9db2b032bc859b71691bf8f
Pablocybernetic <gitaup08@gmail.com>
2024-08-30 00:17:37 +0300

    hghg

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -8,5 +8,8 @@
     <p><strong>Name:</strong> {{ $details['name'] }}</p>
     <p><strong>Email:</strong> {{ $details['email'] }}</p>
     <p><strong>Message:</strong> {{ $details['message'] }}</p>
+    <p><strong>phonenumber:</strong> {{ $details['phonenumber'] }}</p>
+    <p><strong>subject:</strong> {{ $details['subject'] }}</p>
+
 </body>
 </html>
