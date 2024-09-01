3e39fd51ae64603d9a8c5e77489829478e3bd963
Pablocybernetic <gitaup08@gmail.com>
2024-09-01 13:13:31 +0300

    hi

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -331,7 +331,7 @@
                                 <div class="col-lg-12">
                                     <div class="single_post">
                                         <p class="m-b-0">{{ $blog->excerpt }}</p>
-                                         <span class=" text-info">{{ \Carbon\Carbon::parse($blog->created_at)->format('M-d-Y H:i') }}</span>
+                                         <span class=" text-info">{{ \Carbon\Carbon::parse($blog->created_at)->format('l, F j, Y g:i A') }}</span>
                                         <div class="img-post">
                                             <img src="{{ asset($blog->featured_image) }}" alt="Awesome Image">                                            
                                         </div>
